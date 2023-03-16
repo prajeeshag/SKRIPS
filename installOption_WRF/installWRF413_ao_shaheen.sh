1e207ef985a662a6a239ee475419f5a2f7ac7e63
Prajeesh Athippatta <athippp@cdl5.hpc.kaust.edu.sa>
2023-03-16 20:03:32 +0300

    updated

--- installOption_WRF/installWRF413_ao_shaheen.sh
+++ installOption_WRF/installWRF413_ao_shaheen.sh
@@ -15,24 +15,24 @@ echo "nesting option is 1 (normal)"
 printf '50\n1\n' | ./configure &> log.configure
 
 echo "copying other files to compile ESMF--WRF"
-ln -sf ${WRF_UPDATE_DIR0}/Makefile.wrf Makefile
-ln -sf ${WRF_UPDATE_DIR0}/module_domain.F frame/
-ln -sf ${WRF_UPDATE_DIR0}/module_diag_rasm.F phys/
-ln -sf ${WRF_UPDATE_DIR0}/module_ltng_iccg.F phys/
-ln -sf ${WRF_UPDATE_DIR0}/input_wrf.F share/
+cp ${WRF_UPDATE_DIR0}/Makefile.wrf Makefile
+cp ${WRF_UPDATE_DIR0}/module_domain.F frame/
+cp ${WRF_UPDATE_DIR0}/module_diag_rasm.F phys/
+cp ${WRF_UPDATE_DIR0}/module_ltng_iccg.F phys/
+cp ${WRF_UPDATE_DIR0}/input_wrf.F share/
 
-ln -sf ${WRF_UPDATE_DIR0}/ext_esmf_write_field.F90 external/io_esmf/
-ln -sf ${WRF_UPDATE_DIR0}/ext_esmf_read_field.F90 external/io_esmf/
-ln -sf ${WRF_UPDATE_DIR0}/ext_esmf_open_for_read.F90 external/io_esmf/
-ln -sf ${WRF_UPDATE_DIR0}/ext_esmf_open_for_write.F90 external/io_esmf/
-ln -sf ${WRF_UPDATE_DIR0}/module_esmf_extensions.F90 external/io_esmf/
-ln -sf ${WRF_UPDATE_DIR0}/io_esmf.F90 external/io_esmf/
-ln -sf ${WRF_UPDATE_DIR0}/wrf_ESMFMod.F main/
+cp ${WRF_UPDATE_DIR0}/ext_esmf_write_field.F90 external/io_esmf/
+cp ${WRF_UPDATE_DIR0}/ext_esmf_read_field.F90 external/io_esmf/
+cp ${WRF_UPDATE_DIR0}/ext_esmf_open_for_read.F90 external/io_esmf/
+cp ${WRF_UPDATE_DIR0}/ext_esmf_open_for_write.F90 external/io_esmf/
+cp ${WRF_UPDATE_DIR0}/module_esmf_extensions.F90 external/io_esmf/
+cp ${WRF_UPDATE_DIR0}/io_esmf.F90 external/io_esmf/
+cp ${WRF_UPDATE_DIR0}/wrf_ESMFMod.F main/
 
-ln -sf ${WRF_UPDATE_DIR1}/configure.wrf configure.wrf
+cp ${WRF_UPDATE_DIR1}/configure.wrf configure.wrf
 
 echo "compiling WRFv4.1.3"
-./compile  em_real &> log.em_real1
+./compile -j 16 em_real &> log.em_real1
 
 echo "finished copying"
 linenumber=$(grep -n "bundled:" configure.wrf | cut -d : -f 1)
