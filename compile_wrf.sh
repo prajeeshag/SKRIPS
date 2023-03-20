6d2b68ecb56cf4961c4387b9aa8654d89beaf710
Prajeesh Athippatta <athippp@cdl5.hpc.kaust.edu.sa>
2023-03-20 10:53:48 +0300

    updated

--- compile_wrf.sh
+++ compile_wrf.sh
@@ -7,4 +7,6 @@ cd $WRF_DIR
 printf '50\n1\n' | ./configure &> $SKRIPS_DIR/wrf.configure.log
 cp $SKRIPS_DIR/etc/configure.wrf.shaheen_intel configure.wrf
 ./compile $@ em_real 2>&1 | tee $SKRIPS_DIR/wrf.compile.log
+linenumber=$(grep -n "bundled:" configure.wrf | cut -d : -f 1)
+head -n $((linenumber-1)) configure.wrf > configure.wrf_cpl
  
