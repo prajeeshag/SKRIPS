4691f9a3e9a0bf15773c5d31157cdbc2f22cc6ec
Prajeesh Athippatta <athippp@cdl5.hpc.kaust.edu.sa>
2023-03-21 16:38:47 +0300

    Removed PNETCDF dependency

--- compile_wrf.sh
+++ compile_wrf.sh
@@ -5,6 +5,7 @@ export SKRIPS_DIR=${SKRIPS_DIR:-$(pwd)}
 . $SKRIPS_DIR/etc/env.shaheen_intel
 cd $WRF_DIR
 printf '50\n1\n' | ./configure &> $SKRIPS_DIR/wrf.configure.log
+cp configure.wrf configure.wrf_org
 cp $SKRIPS_DIR/etc/configure.wrf.shaheen_intel configure.wrf
 ./compile $@ em_real 2>&1 | tee $SKRIPS_DIR/wrf.compile.log
 linenumber=$(grep -n "bundled:" configure.wrf | cut -d : -f 1)
