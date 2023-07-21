cef10006bee07de296f440209c944cc3ad4431c2
Prajeesh Ag <athippp@cdl3.hpc.kaust.edu.sa>
2023-07-21 17:28:07 +0300

    some updates, but model hanging at coupler

--- src/coupler/mod_esmf_atm.F90
+++ src/coupler/mod_esmf_atm.F90
@@ -18,14 +18,13 @@ module mod_esmf_atm
   use module_wrf_top, only: wrf_init, wrf_run, wrf_finalize
   use module_domain, only: head_grid, get_ijk_from_grid
   use module_utility, only: WRFU_TIME, WRFU_TIMESET, WRFU_TIMEGET
-  use module_wrf_quilt, only: compute_node
 
   implicit none
   private
 
 
   integer :: NX, NY  ! Global NX, NY
-  integer :: isc=0, iec=-1, jsc=0, jec=-1 ! Local bounds
+  integer :: isc, iec, jsc, jec ! Local bounds
 
   integer :: localPet, petCount
   character(ESMF_MAXSTR) :: component_name
@@ -168,33 +167,22 @@ contains
     ! Initialize the gridded component
     !-------------------------------------------------------------------
     !
-
-    call ESMF_VMBarrier(vm,rc=rc)
-    print *, "BEFORE WRF_INIT in __FILE__ on line __LINE__"
-
     call wrf_set_dm_communicator(comm)
     call wrf_init()
-   
-    call ESMF_VMBarrier(vm,rc=rc)
-    print *, "After WRF_INIT in __FILE__ on line __LINE__"
-
-    if (compute_node) then
-      call get_ijk_from_grid( &
-        head_grid ,                   &
-        ids, ide, jds, jde, kds, kde,    &
-        ims, ime, jms, jme, kms, kme,    &
-        ips, ipe, jps, jpe, kps, kpe  &
-      )
-      
-      NX = ide - ids  ! in other words, the number of points from ids to ide-1 inclusive
-      NY = jde - jds  ! in other words, the number of points from jds to jde-1 inclusive
-      
-      isc = ips
-      jsc = jps
-      iec = MIN(ipe, ide - 1)
-      jec = MIN(jpe, jde - 1)
-    endif
-
+    call get_ijk_from_grid( &
+      head_grid ,                   &
+      ids, ide, jds, jde, kds, kde,    &
+      ims, ime, jms, jme, kms, kme,    &
+      ips, ipe, jps, jpe, kps, kpe  &
+    )
+    
+    NX = ide - ids  ! in other words, the number of points from ids to ide-1 inclusive
+    NY = jde - jds  ! in other words, the number of points from jds to jde-1 inclusive
+    
+    isc = ips
+    jsc = jps
+    iec = MIN(ipe, ide - 1)
+    jec = MIN(jpe, jde - 1)
 
     call ATM_SetGridArrays(gcomp, atmGridIn, rc)
     _ERR_CHK(__FILE__,__LINE__)
@@ -349,14 +337,12 @@ contains
     type(ESMF_Array) :: arrX, arrY
     type(ESMF_StaggerLoc) :: staggerLoc
     type(ESMF_DistGrid) :: distGrid
-    type(ESMF_DELayout) :: deLayout
     integer, allocatable :: deBlockList(:, :, :)
-    integer :: j, i, k, de, deCount
+    integer :: j, i, k
     integer, allocatable :: isc_global(:)
     integer, allocatable :: jsc_global(:)
     integer, allocatable :: iec_global(:)
     integer, allocatable :: jec_global(:)
-    integer, allocatable :: petMap(:)
 
 
     ! Local variable declarations
@@ -370,7 +356,7 @@ contains
     if (allocated(iec_global)) deallocate(iec_global); allocate(iec_global(petCount))
     if (allocated(jsc_global)) deallocate(jsc_global); allocate(jsc_global(petCount))
     if (allocated(jec_global)) deallocate(jec_global); allocate(jec_global(petCount))
-
+    
     call ESMF_VMAllGatherV(&
       vm, sendData=[isc], &
       sendCount=1, &
@@ -412,30 +398,22 @@ contains
     _ERR_CHK(__FILE__,__LINE__)
     
     
-    deCount = count(isc_global > 0) ! isc is 0 for ioservers
-    allocate (deBlockList(2, 2, 1:deCount))
-    allocate (petMap(1:deCount))
-    
-    de = 0 
+    ! Get limits of the grid arrays (based on PET and nest level)
+    if (allocated(deBlockList)) deallocate(deBlockList)
+    allocate (deBlockList(2, 2, 1:petCount))
+
     do tile = 1, petCount
-      if (isc_global(tile) < 1) continue
-      de = de + 1
-      deBlockList(1, 1, de) = isc_global(tile)
-      deBlockList(1, 2, de) = iec_global(tile) 
-      deBlockList(2, 1, de) = jsc_global(tile) 
-      deBlockList(2, 2, de) = jec_global(tile)
-      petMap(de) = tile-1
+      deBlockList(1, 1, tile) = isc_global(tile)
+      deBlockList(1, 2, tile) = iec_global(tile) 
+      deBlockList(2, 1, tile) = jsc_global(tile) 
+      deBlockList(2, 2, tile) = jec_global(tile) 
     end do
-    
-    deLayout = ESMF_DELayoutCreate(petMap, rc=rc)
-    _ERR_CHK(__FILE__,__LINE__)
-    
+
     ! Create ESMF DistGrid based on model domain decomposition
     distGrid = ESMF_DistGridCreate( &
       minIndex=(/1, 1/), &
       maxIndex=(/NX, NY/), &
       deBlockList=deBlockList, &
-      deLayout=deLayout, &
       rc=rc &
     )
     _ERR_CHK(__FILE__,__LINE__)
@@ -449,51 +427,42 @@ contains
                              name="atm_grid", rc=rc)
     _ERR_CHK(__FILE__,__LINE__)
 
-    deallocate(isc_global)
-    deallocate(iec_global)
-    deallocate(jsc_global)
-    deallocate(jec_global)
-    deallocate(deBlockList)
-    deallocate(petMap)
-
     ! Allocate coordinates
     call ESMF_GridAddCoord(gridIn, staggerLoc=staggerLoc, rc=rc)
     _ERR_CHK(__FILE__,__LINE__)
-    
-    if (compute_node) then
-      call ESMF_GridGetCoord(&
-        gridIn, localDE=0, &
-        staggerLoc=staggerLoc, &
-        coordDim=1, &
-        farrayPtr=ptrX, &
-        rc=rc)
-      _ERR_CHK(__FILE__,__LINE__)
 
-      call ESMF_GridGetCoord(&
-        gridIn, localDE=0, &
-        staggerLoc=staggerLoc, &
-        coordDim=2, &
-        farrayPtr=ptrY, &
-        rc=rc)
-      _ERR_CHK(__FILE__,__LINE__)
+    call ESMF_GridGetCoord(&
+      gridIn, localDE=0, &
+      staggerLoc=staggerLoc, &
+      coordDim=1, &
+      farrayPtr=ptrX, &
+      rc=rc)
+    _ERR_CHK(__FILE__,__LINE__)
+
+    call ESMF_GridGetCoord(&
+      gridIn, localDE=0, &
+      staggerLoc=staggerLoc, &
+      coordDim=2, &
+      farrayPtr=ptrY, &
+      rc=rc)
+    _ERR_CHK(__FILE__,__LINE__)
  
-      do j = jsc, jec
-        do i = isc, iec
-          ptrY(i,j) = head_grid%xlat(i,j)
-          if (head_grid%xlong(i, j) < 0.) then
-            ptrX(i, j) = head_grid%xlong(i, j) + 360d0
-          else
-            ptrX(i, j) = head_grid%xlong(i, j)
-          end if
-        end do
+    do j = jsc, jec
+      do i = isc, iec
+        ptrY(i,j) = head_grid%xlat(i,j)
+        if (head_grid%xlong(i, j) < 0.) then
+          ptrX(i, j) = head_grid%xlong(i, j) + 360d0
+        else
+          ptrX(i, j) = head_grid%xlong(i, j)
+        end if
       end do
+    end do
 
-      ! Nullify pointers
-      if (associated(ptrY)) nullify (ptrY)
-      if (associated(ptrX)) nullify (ptrX)
-    end if ! if (compute_node) 
+    ! Nullify pointers
+    if (associated(ptrY)) nullify (ptrY)
+    if (associated(ptrX)) nullify (ptrX)
   
-      ! Write the grid to debug
+    ! Write the grid to debug
     if (debugLevel >= 1) then
       call ESMF_GridGetCoord(gridIn, &
                              staggerLoc=ESMF_STAGGERLOC_CENTER, &
@@ -510,9 +479,15 @@ contains
                            status=ESMF_FILESTATUS_NEW, rc=rc)
       _ERR_CHK(__FILE__,__LINE__)
     end if
+    
     ! Assign grid to gridded component
     call ESMF_GridCompSet(gcomp, grid=gridIn, rc=rc)
     _ERR_CHK(__FILE__,__LINE__)
+    if (allocated(isc_global)) deallocate(isc_global)
+    if (allocated(iec_global)) deallocate(iec_global)
+    if (allocated(jsc_global)) deallocate(jsc_global)
+    if (allocated(jec_global)) deallocate(jec_global)
+    if (allocated(deBlockList)) deallocate(deBlockList)
 
   end subroutine ATM_SetGridArrays
 
@@ -572,17 +547,16 @@ contains
         indexflag=ESMF_INDEX_GLOBAL, &
         name=entryNameWRF, rc=rc)
 
-      if (compute_node) then
-        !Put initial data into state
-        call ESMF_FieldGet(field_tmp, localDe=0, farrayPtr=ptr2d_tmp, rc=rc)
-        _ERR_CHK(__FILE__,__LINE__)
 
-        ! Initialize pointer
-        ptr2d_tmp = 0.d0
-        ! Nullify the pointer
-        if (associated(ptr2d_tmp)) then
-          nullify (ptr2d_tmp)
-        end if
+      !Put initial data into state
+      call ESMF_FieldGet(field_tmp, localDe=0, farrayPtr=ptr2d_tmp, rc=rc)
+      _ERR_CHK(__FILE__,__LINE__)
+
+      ! Initialize pointer
+      ptr2d_tmp = 0.d0
+      ! Nullify the pointer
+      if (associated(ptr2d_tmp)) then
+        nullify (ptr2d_tmp)
       end if
 
       if (ATMtoOCN(iEntry)) then
@@ -618,7 +592,7 @@ contains
     type(ESMF_State) :: esmfState
 
     rc = ESMF_SUCCESS
-    
+
     call ESMF_GridCompGet(&
       gcomp, name=component_name, &
       clock=clock, &
@@ -626,8 +600,6 @@ contains
       exportState=esmfState, &
       vm=vm, rc=rc)
     _ERR_CHK(__FILE__,__LINE__)
-
-    if (.not. compute_node) return
 
     ! Put field 1
     _ATM_PUT(LANDMASK)
@@ -701,7 +673,6 @@ contains
 
     rc = ESMF_SUCCESS
 
-
     call ESMF_GridCompGet(&
       gcomp, &
       name=component_name, &
@@ -710,7 +681,6 @@ contains
       importState=esmfState, &
       vm=vm, rc=rc)
     _ERR_CHK(__FILE__,__LINE__)
-    if (.not. compute_node) return
 
     _ATM_GET(SST)
     _ATM_GET(UOCE)
