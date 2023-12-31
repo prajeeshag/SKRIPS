6d2cb28bf8c2db5a68a54c56e4176873774538aa
Prajeesh Ag <athippp@cdl4.hpc.kaust.edu.sa>
2023-12-31 09:02:51 +0300

    updated dl_ecmwf_seasrfrcst

--- tools/misc/dl_seas_rfor_ecmwf.py
+++ tools/misc/dl_seas_rfor_ecmwf.py
@@ -13,14 +13,14 @@ def download(date: str, dtype: str, endhour: int = 5160, nmem: int = 25, test: b
 
     server = ECMWFService("mars")
 
-    minStarth=24
+    minStarth=0
     maxEndh=endhour
     endh=min(5160,maxEndh)
 
 
 
     if dtype=='pl':
-        starth=max(12,minStarth)
+        starth=minStarth
         inth=12
         step='/'.join(map(str,list(range(starth,endh+inth,inth))))
         if test:
@@ -39,26 +39,26 @@ def download(date: str, dtype: str, endhour: int = 5160, nmem: int = 25, test: b
                     number="/".join(map(str,list(range(0,nnmem))))
 
                 server.execute({
-                    "class": "c3",
+                    "class": "od",
                     "date": date,
                     "expver": "1",
-                    "levelist": "10/30/50/100/200/300/400/500/700/850/925/1000",
+                    "levelist": "10/30/50/70/100/200/300/400/500/700/850/925/1000",
                     "levtype": "pl",
                     "method": "1",
                     "number": number,
                     "origin": "ecmf",
-                    "param": "129.128/130.128/131.128/132.128/133.128",
+                    "param": "129.128/130.128/131/132/133.128",
                     "step": step,
                     "stream": "mmsf",
-                    "system": "51",
+                    "system": "5",
                     "time": "00:00:00",
                     "type": "fc",
                     },f"pl_{date}_{i}.grib"
                 )
 
     elif dtype=='sfc':
-        starth=max(0,minStarth)
-        inth=6
+        starth=minStarth
+        inth=12
         step='/'.join(map(str,list(range(starth,endh+inth,inth))))
         nnmem=min(MAXMEM,nmem)
         number="/".join(map(str,list(range(0,nnmem))))
@@ -67,50 +67,20 @@ def download(date: str, dtype: str, endhour: int = 5160, nmem: int = 25, test: b
             print(step)
         else:
             server.execute({
-                "class":"c3",
+                "class":"od",
                 "date":date,
                 "expver":"1",
                 "levtype":"sfc",
                 "method":"1",
                 "number":number,
                 "origin":"ecmf",
-                #"param":"34.128/129.128/139.128/151.128/165.128/166.128/167.128/168.128/172.128",
-                "param":"34.128/139.128/151.128/165.128/166.128/167.128/168.128",
+                "param":"34.128/39.128/40.128/41.128/42.128/139.128/151.128/165.128/166.128/167.128/168.128/170.128/183.128/236.128",
                 "step": step,
                 "stream":"mmsf",
-                "system":"51",
+                "system":"5",
                 "time":"00:00:00",
                 "type":"fc",
                 },f"sfc_{date}.grib"
-            )
-
-    elif dtype=='soil':
-
-        starth=max(24,minStarth)
-        inth=24
-        step='/'.join(map(str,list(range(starth,endh+inth,inth))))
-        nnmem=min(MAXMEM,nmem)
-        number="/".join(map(str,list(range(0,nnmem))))
-
-        if test:
-            print(step)
-        else:
-            server.execute({
-                "class":"c3",
-                "date":date,
-                "expver":"1",
-                "levelist":"1/2/3/4",
-                "levtype":"sol",
-                "method":"1",
-                "number": number,
-                "origin":"ecmf",
-                "param":"260199",
-                "step": step,
-                "stream":"mmsf",
-                "system":"51",
-                "time":"00:00:00",
-                "type":"fc",
-                },f"soil_{date}.grib"
             )
 
 
