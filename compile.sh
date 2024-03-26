ef1d9447515166c1bad1a1ca222b8fbdfd7ebb62
Prajeesh Ag <athippp@login4.hsn.shaheen.hpc.kaust.edu.sa>
2024-03-26 12:11:04 +0300

    Porting to Shaheen3

--- compile.sh
+++ compile.sh
@@ -12,8 +12,18 @@ function __get_absolute_path {
 
 function __source_env {
     if [ -f "env.mach" ]; then
-        export SKRIPS_DIR=$(pwd)
         source env.mach
+        export SKRIPS_DIR=$(pwd)
+        export MITGCM_DIR=$SKRIPS_DIR/external/MITgcm/
+        export WRF_DIR=$SKRIPS_DIR/external/WRF/
+        export JASPER_DIR=$SKRIPS_DIR/external/jasper/
+        export WPS_DIR=$SKRIPS_DIR/external/WPS/
+        export ESMF_DIR=$SKRIPS_DIR/external/esmf/
+        export ESMF_LIB=$ESMF_DIR/lib/lib$ESMF_BOPT/$ESMF_OS.$ESMF_COMPILER.$ESMF_ABI.$ESMF_COMM.$ESMF_SITE/
+        export ESMF_MOD=$ESMF_DIR/mod/mod$ESMF_BOPT/$ESMF_OS.$ESMF_COMPILER.$ESMF_ABI.$ESMF_COMM.$ESMF_SITE/ 
+        export ESMFMKFILE=$ESMF_LIB/esmf.mk
+        export JASPERLIB=$SKRIPS_DIR/libs/jasper/lib64
+        export JASPERINC=$SKRIPS_DIR/libs/jasper/include
     else
         echo "No env file found"
         echo "NOTE: try again after running: $0 set_machine MACHINENAME "
@@ -66,6 +76,66 @@ function __build_esmf_lib {
     time make -j $jobs 2>&1 | tee $SKRIPS_DIR/esmf.compile.log
 }
 
+function __build_jasper {
+
+    jobs=$1
+    echo "Building JASPER...."
+    cd $JASPER_DIR
+    buildDir=$SKRIPS_DIR/build/jasper
+    mkdir -p $buildDir
+    prefix=$SKRIPS_DIR/libs
+
+    cmake -G "Unix Makefiles" \
+      -H$JASPER_DIR -B$buildDir \
+      -DCMAKE_INSTALL_PREFIX=$prefix \
+      -DCMAKE_BUILD_TYPE=RELEASE \
+      -DJAS_ENABLE_DOC=FALSE \
+      -DCMAKE_POLICY_DEFAULT_CMP0074=NEW \
+
+    cd $buildDir && make -j $jobs && make install
+}
+
+function __build_wps {
+    jobs=$1
+    clean=$2
+    cd $WPS_DIR
+
+    if [[ $clean -eq 1 ]]; then 
+      ./clean -a 
+      return 
+    fi
+   
+    printf $WPS_CONFIG_OPT 
+    printf $WPS_CONFIG_OPT | ./configure 2>&1 | tee $SKRIPS_DIR/wps.configure.log
+    wpsconfig=$SKRIPS_DIR/etc/$WPSCONFIGURE_FILE
+
+    if [ -f $wpsconfig ]; then
+      cp $wpsconfig configure.wps
+    fi
+
+    time ./compile 2>&1 | tee $SKRIPS_DIR/wps.compile.log
+}
+
+function build_jasper {
+    __addarg "-h" "--help" "help" "optional" "" "Build JASPER"
+    __addarg "-j" "--jobs" "storevalue" "optional" "4" "Allow N parallel jobs at once"
+    __parseargs "$@"
+
+    __source_env
+    __build_jasper $jobs 
+}
+
+function build_wps {
+    __addarg "-h" "--help" "help" "optional" "" "Build WPS"
+    __addarg "-j" "--jobs" "storevalue" "optional" "4" "Allow N parallel jobs at once"
+    __addarg "" "--clean" "flag" "optional" "" "make clean"
+    __parseargs "$@"
+
+    __source_env
+    __build_wps $jobs $clean
+}
+
+
 function build_esmf_lib {
     __addarg "-h" "--help" "help" "optional" "" "Build the ESMF library"
     __addarg "-j" "--jobs" "storevalue" "optional" "4" "Allow N parallel jobs at once"
@@ -88,6 +158,12 @@ function __build_wrf_lib {
     fi
     
     printf $WRF_CONFIG_OPT | ./configure 2>&1 | tee $SKRIPS_DIR/wrf.configure.log
+    
+    wrfconfig=$SKRIPS_DIR/etc/$WRFCONFIGURE_FILE
+
+    if [ -f $wrfconfig ]; then
+      cp $wrfconfig configure.wrf
+    fi
     rm -rf external/esmf_time_f90 && ln -sf $SKRIPS_DIR/external/cesmf_time_f90 external/esmf_time_f90
 
     if [[ $debug -eq 1 ]]; then
@@ -99,7 +175,7 @@ function __build_wrf_lib {
     head -n $((linenumber-1)) configure.wrf > configure.wrf_cpl
 }
 
-function build_wrf_lib {
+function build_wrf {
     __addarg "-h" "--help" "help" "optional" "" "Build the WRF as library"
     __addarg "-j" "--jobs" "storevalue" "optional" "16" "Allow N parallel jobs at once"
     __addarg "" "--clean" "flag" "optional" "" "clean the build"
